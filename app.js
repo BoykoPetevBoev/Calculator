2e9aad6a26d2fdb1bfa15dd2dd279bab1b7c1a13
Бойко Боев <boykopetevboev@gmail.com>
2020-02-03 20:16:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,58 +2,71 @@ const body = document.getElementById('body');
 const result1 = document.getElementById('result1');
 const result2 = document.getElementById('result2');
 
-const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const operators = ['+', '-', 'x', '/'];
-
+const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const operators = {
+    '+': () => result = Number(a) + Number(b),
+    '-': () => result = Number(a) - Number(b),
+    'x': () => result = Number(a) * Number(b),
+    '/': () => result = Number(a) / Number(b)
+}
 let operatorClicked = false;
-let end = false;
+let endCalculation = false;
 let operator = '';
-let num = '';
-let a = 0;
-let b = 0;
+let a = '';
+let b = '';
+let result = 0;
 
-body.addEventListener('click', function (ctx) {
-    const value = ctx.target.value;
-    console.log(value)
-    if (nums.includes(value) && !end) {
-        result1.textContent += value;
-        num += value;
+body.addEventListener('click', function (e) {
+    const value = e.target.value;
+    if (nums.includes(value) && !endCalculation && !operatorClicked) {
+        a = createVariable(a, value);
+        showInfo();
     }
-    else if (operators.includes(value) && !operatorClicked && num != '' && !end) {
+    if (nums.includes(value) && !endCalculation && operatorClicked) {
+        b = createVariable(b, value);
+        showInfo();
+    }
+    else if (operators.hasOwnProperty(value) && !operatorClicked && a != '' && !endCalculation) {
         operator = value;
-        result1.textContent += value;
-        a = Number(num);
-        num = '';
+        operatorClicked = true;
+        showInfo();
+    }
+    else if (value === '=' && b != '' && operatorClicked && !endCalculation) {
+        endCalculation = true;
+        operators[operator]();
+        printResult();
+    }
+    else if (operators.hasOwnProperty(value) && endCalculation) {
+        a = result;
+        b = '';
+        operator = value;
+        showInfo();
+        result2.textContent = '';
+        endCalculation = false;
         operatorClicked = true;
     }
-    else if (value === '=' && num != '' && operatorClicked && !end) {
-        end = true;
-        b = Number(num);
-        printResult(a, b, operator);
-    }
-    else if(value === 'clear'){
+    else if (value === 'clear') {
         result1.textContent = '';
         result2.textContent = '';
-        a = 0;
-        b = 0;
+        a = '';
+        b = '';
+        result = 0;
         operator = '';
         operatorClicked = false;
-        num = '';
-        end = false;
+        endCalculation = false;
     }
 });
-function printResult(a, b, operator) {
-    console.log(a, b, operator)
-    if(operator == '+'){
-        result2.textContent += ` = ${a + b}`;
+function createVariable(x, value){
+    if (value === '.' && !x.includes(value) && x != '') {
+        x += value;
+    } else if (value != '.') {
+        x += value;
     }
-    else if(operator == '-'){
-        result2.textContent += `= ${a - b}`;
-    }
-    else if(operator == 'x'){
-        result2.textContent += `= ${a * b}`;
-    }
-    else if(operator == '/'){
-        result2.textContent += `= ${a / b}`;
-    }
+    return x;
+}
+function showInfo() {
+    result1.textContent = `${a} ${operator} ${b}`;
+}
+function printResult() {
+    result2.textContent = result;
 }
