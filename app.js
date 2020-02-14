96dcfc916539434fab26eeb8388370f71416f085
Бойко Боев <boykopetevboev@gmail.com>
2020-02-14 15:29:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const body = document.getElementById('body');
 const result1 = document.getElementById('result1');
 const result2 = document.getElementById('result2');
 
-const nums = ['00', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const operators = {
+const validBtns = ['00', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const resultCalculations = {
     '+': () => result = Number(a) + Number(b),
     '-': () => result = Number(a) - Number(b),
     'x': () => result = Number(a) * Number(b),
@@ -31,49 +31,77 @@ document.addEventListener('keyup', function (e) {
     eventHandler(value);
 });
 function eventHandler(value) {
-    if (nums.includes(value) && !endCalculation && !operatorClicked) {
+    const conditionA = validBtns.includes(value) && !endCalculation && !operatorClicked;
+    const conditionB = validBtns.includes(value) && !endCalculation && operatorClicked;
+    const conditionOperator = resultCalculations.hasOwnProperty(value) && !operatorClicked && a != '' && !endCalculation;
+    const conditionForEnd = value === '=' && b != '' && operatorClicked && !endCalculation;
+    const conditionForDelete = value === 'CE' && endCalculation === false;
+    const conditionForContinueCalc = resultCalculations.hasOwnProperty(value) && endCalculation;
+    if (conditionA) {
         a = createVariable(a, value);
         showInfo();
     }
-    if (nums.includes(value) && !endCalculation && operatorClicked) {
+    else if (conditionB) {
         b = createVariable(b, value);
         showInfo();
     }
-    else if (operators.hasOwnProperty(value) && !operatorClicked && a != '' && !endCalculation) {
+    else if (conditionOperator) {
         operator = value;
         operatorClicked = true;
         showInfo();
     }
-    else if (value === '=' && b != '' && operatorClicked && !endCalculation) {
+    else if (conditionForEnd) {
         endCalculation = true;
-        operators[operator]();
+        resultCalculations[operator]();
         printResult();
     }
-    else if (operators.hasOwnProperty(value) && endCalculation) {
-        a = result;
+    else if (conditionForContinueCalc) {
+        continueCalc();
+    }
+    else if (value === 'clear') {
+        clearCalculation();
+    }
+    else if (conditionForDelete) {
+        backspace();
+    }
+}
+function continueCalc(){
+    a = result;
         b = '';
         operator = value;
         showInfo();
         result2.textContent = '';
         endCalculation = false;
         operatorClicked = true;
-    }
-    else if (value === 'clear') {
-        if(a !== '' && operator !== '' && b !== '' && result != 0){
-            historyArr.unshift(`${a} ${operator} ${b} = ${result}`);
-            printHistory();
-        }
-        result1.textContent = '';
-        result2.textContent = '';
-        a = '';
-        b = '';
-        result = 0;
-        operator = '';
-        operatorClicked = false;
-        endCalculation = false;
-    }
 }
-function printHistory(){
+function backspace(){
+    if (b != '') {
+            b = b.substring(0, b.length - 1)
+        }
+        else if(operator !== ''){
+            operator = '';
+            operatorClicked = false;
+        }
+        else if(a !== ''){
+            a = a.substring(0, a.length - 1)
+        }
+        showInfo()
+}
+function clearCalculation(){
+    if (a !== '' && operator !== '' && b !== '' && result != 0) {
+        historyArr.unshift(`${a} ${operator} ${b} = ${result}`);
+        printHistory();
+    }
+    result1.textContent = '';
+    result2.textContent = '';
+    a = '';
+    b = '';
+    result = 0;
+    operator = '';
+    operatorClicked = false;
+    endCalculation = false;
+}
+function printHistory() {
     const history = document.getElementById('historyDiv');
     history.innerHTML = '<h2></h2>';
     historyArr.forEach(element => {
@@ -83,7 +111,7 @@ function printHistory(){
     });
 }
 function createVariable(x, value) {
-    if(value === '.' && !x.includes(value) && x === ''){
+    if (value === '.' && !x.includes(value) && x === '') {
         x += `0${value}`;
     }
     else if (value === '.' && !x.includes(value) && x != '') {
