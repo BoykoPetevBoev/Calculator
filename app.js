e347e310e4613ff2e42d180ba156e0707748bbba
Бойко Боев <boykopetevboev@gmail.com>
2020-02-17 19:14:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,6 @@
-const calculator = document.getElementById('calculator');
-const result1 = document.getElementById('result1');
-const result2 = document.getElementById('result2');
-
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const seven = document.getElementById('seven');
-const eigh = document.getElementById('eigh');
-const nine = document.getElementById('nine');
-const ten = document.getElementById('ten');
-const divNumbers = [one, two, three, four, five, six, seven, eigh, nine, ten];
-
 startTime();
-const validBtns = ['00', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const resultCalculations = {
-    '+': () => result = Number(a) + Number(b),
-    '-': () => result = Number(a) - Number(b),
-    'x': () => result = Number(a) * Number(b),
-    '/': () => result = Number(a) / Number(b),
-    '*': () => result = Number(a) * Number(b),
-}
 
-let operatorClicked = false;
-let endCalculation = false;
-let operator = '';
-let a = '';
-let b = '';
-let result = 0;
-const maxLengthNum = 16;
-
+const calculator = document.getElementById('calculator');
 calculator.addEventListener('click', function (e) {
     const value = e.target.value;
     eventHandler(value);
@@ -41,81 +10,132 @@ document.addEventListener('keyup', function (e) {
     if (value == 'Enter') {
         value = '=';
     }
+    else if (value == '*') {
+        value = 'x';
+    }
+    else if (value == 'Backspace') {
+        value = 'CE'
+    }
+    else if (value == 'Delete') {
+        value = 'clear'
+    }
     eventHandler(value);
 });
+const validBtns = ['00', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+let result = 0;
+const resultCalculations = {
+    '+': () => result = Number(a) + Number(b),
+    '-': () => result = Number(a) - Number(b),
+    'x': () => result = Number(a) * Number(b),
+    '/': () => result = Number(a) / Number(b)
+}
+let end = false;
+let operator = '';
+let a = '';
+let b = '';
+const maxLengthNum = 16;
+
 function eventHandler(value) {
-    const conditionA = validBtns.includes(value) && !endCalculation && !operatorClicked && a.length < maxLengthNum;
-    const conditionB = validBtns.includes(value) && !endCalculation && operatorClicked && b.length < maxLengthNum;
-    const conditionOperator = resultCalculations.hasOwnProperty(value) && !operatorClicked && a != '' && !endCalculation;
-    const conditionForEnd = value === '=' && b != '' && operatorClicked && !endCalculation;
-    const conditionForDelete = value === 'CE' && endCalculation === false;
-    const conditionForContinueCalc = resultCalculations.hasOwnProperty(value) && endCalculation;
-    if (conditionA) {
-        a = createVariable(a, value);
-        showInfo();
+    console.log(value)
+    const conditionFractionalExpression = end == false && a !== '' && operator == '' && (value == '½' || value == '⅓' || value == '⅔' || value == '¾' || value == '⅞');
+    if (validBtns.includes(value) || resultCalculations.hasOwnProperty(value)) {
+        createExpression(value);
     }
-    else if (conditionB) {
-        b = createVariable(b, value);
-        showInfo();
-    }
-    else if (conditionOperator) {
-        operator = value;
-        operatorClicked = true;
-        showInfo();
-    }
-    else if (conditionForEnd) {
-        endCalculation = true;
-        resultCalculations[operator]();
-        printResult();
-    }
-    else if (conditionForContinueCalc) {
-        continueCalc(value);
+    else if (value === '=') {
+        endCalculation();
     }
     else if (value === 'clear') {
         clearCalculation();
     }
-    else if (conditionForDelete) {
+    else if (value === 'CE' && end === false) {
         backspace();
     }
-}
-function printTable() {
-    for (let i in divNumbers) {
-        let div = divNumbers[i];
-        const h3 = document.createElement('h3')
-
-        for (let j of divNumbers) {
-            const num = Number(i) + 1;
-            const p = document.createElement('p');
-            div = divNumbers[i];
-            p.textContent = `${num} x ${divNumbers.indexOf(j) + 1} = ${Number(num) * Number(divNumbers.indexOf(j) + 1)}`;
-            div.appendChild(p);
+    else if (value === '±' && end === false) {
+        changeSign();
+    }
+    else if (conditionFractionalExpression) {
+        calculateFractionalExpression(value);
+    }
+    else if(value == 'π' && end === false){
+        if(a == ''){
+            a = Math.PI;
+            showExpression(a);
         }
+        else if(a !== '' && operator !== '' && b === ''){
+            b = Math.PI;
+            showExpression(`${a} ${operator} ${b}`);
+        }
+    }
+    else if(value === '%' && end === false){
+        
+    }
+}
+function createExpression(value) {
+    const conditionA = end === false && validBtns.includes(value) && operator === '' && a.length < maxLengthNum;
+    const conditionB = end === false && validBtns.includes(value) && operator !== '' && b.length < maxLengthNum;
+    const conditionOperator = end === false && resultCalculations.hasOwnProperty(value) && a != '' && b == '';
+    const conditionContinue = end === true && resultCalculations.hasOwnProperty(value);
+    if (conditionA) {
+        a = createVariable(a, value);
+        showExpression(a);
+    }
+    else if (conditionB) {
+        b = createVariable(b, value);
+        showExpression(`${a} ${operator} ${b}`);
+    }
+    else if (conditionOperator) {
+        operator = value;
+        a = checkEnd(a);
+        showExpression(`${a} ${operator}`);
+    }
+    else if (conditionContinue) {
+        continueCalc(value);
+    }
+}
+function checkEnd(num) {
+    if (num.toString().endsWith('.')) {
+        num = num.replace('.', '');
+    }
+    return num;
+}
+function showExpression(expression) {
+    const result1 = document.getElementById('result1');
+    result1.textContent = expression;
+}
+function endCalculation() {
+    const conditionEnd = b != '' && operator !== '' && end === false;
+    if (conditionEnd) {
+        b = checkEnd(b);
+        end = true;
+        resultCalculations[operator]();
+        showExpression(`${a} ${operator} ${b}`);
+        printResult(`= ${result}`);
     }
 }
 function continueCalc(value) {
-    a = result;
+    a = result.toString();
     b = '';
     operator = value;
-    showInfo();
-    result2.textContent = '';
-    endCalculation = false;
-    operatorClicked = true;
+    end = false;
+    showExpression(`${a} ${operator}`);
+    printResult('');
 }
 function backspace() {
     if (b != '') {
-        b = b.substring(0, b.length - 1)
+        b = b.toString();
+        b = b.substring(0, b.length - 1);
     }
     else if (operator !== '') {
         operator = '';
-        operatorClicked = false;
     }
     else if (a !== '') {
-        a = a.substring(0, a.length - 1)
+        a = a.toString();
+        a = a.substring(0, a.length - 1);
     }
-    showInfo()
+    showExpression(`${a} ${operator} ${b}`);
 }
 function clearCalculation() {
-    if (a !== '' && operator !== '' && b !== '' && result != 0) {
+    if (a !== '' && operator !== '' && b !== '' && end == true) {
         const line = `${a} ${operator} ${b} = ${result}`;
         printHistory(line);
     }
@@ -125,52 +145,58 @@ function clearCalculation() {
     b = '';
     result = 0;
     operator = '';
-    operatorClicked = false;
-    endCalculation = false;
-}
-
-function printHistory(line) {
-    const history = document.getElementById('historyDiv');
-    const h3 = document.createElement('h3');
-    h3.innerHTML = line;
-    history.appendChild(h3);
+    end = false;
 }
 function createVariable(x, value) {
     if (value === '.' && !x.includes(value) && x === '') {
         x += `0${value}`;
     }
-    else if (value === '.' && !x.includes(value) && x != '') {
+    else if (value === '.' && !x.includes(value) && x !== '') {
         x += value;
-    } else if (value != '.') {
+    }
+    else if (value != '.') {
         x += value;
     }
     return x;
 }
-function showInfo() {
-    result1.textContent = `${a} ${operator} ${b}`;
-}
-function printResult() {
-    result2.textContent = '= ' + result;
-}
-function startTime() {
-    const today = new Date();
-
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = today.getMonth() + 1;
-    let yyyy = today.getFullYear();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    dd = checkTime(dd);
-    mm = checkTime(mm);
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML = `${h}:${m}:${s} ${dd}.${mm}.${yyyy} `;
-    var t = setTimeout(startTime, 500);
-    function checkTime(i) {
-        i = Number(i);
-        if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
-        return i;
+function changeSign() {
+    if (a !== '' && operator === '') {
+        a = Number(a) * (-1)
+        showExpression(a);
+    }
+    else if (operator === '+') {
+        operator = '-';
+        showExpression(`${a} ${operator} ${b}`);
+    }
+    else if (operator === '-') {
+        operator = '+';
+        showExpression(`${a} ${operator} ${b}`);
     }
 }
+function calculateFractionalExpression(FE) {
+    const expression = {
+        '½': () => result = Number(a) / 2,
+        '⅓': () => result = Number(a) / 3,
+        '⅔': () => result = (Number(a) * 2) / 3,
+        '¾': () => result = (Number(a) * 3) / 4,
+        '⅞': () => result = (Number(a) * 7) / 8
+    }
+    operator = '';
+    b = FE;
+    expression[FE]();
+    end = true;
+    showExpression(`${a} ${operator} ${b}`);
+    printResult(`= ${result}`);
+}
+function printResult(result) {
+    const result2 = document.getElementById('result2');
+    result2.textContent = result;
+}
+function printHistory(line) {
+    const history = document.getElementById('historyDiv');
+    const p = document.createElement('p');
+    p.innerHTML = line;
+    history.appendChild(p);
+}
+
 
